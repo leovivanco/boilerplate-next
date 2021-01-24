f043d4cbe1c579bfd2a174c5b41718ce365ae146
Leonardo Vivanco <leonardo.vivanco@wetek.com>
2021-01-24 21:48:40 +0000

    Update covarge typescript

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules', '/.next/'],
   colletCoverage: true,
-  colletCoverageFrom: ['src/**/*/.ts(x)'],
+  colletCoverageFrom: ['src/**/*/.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
